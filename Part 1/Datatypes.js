c841defaa369dac67fe8d1d54111c7cc09680cd4
Shaurya <shaurya.35k@gmail.com>
2025-10-25 11:25:27 +0530

    datatypes

--- Part 1/Datatypes.js
+++ Part 1/Datatypes.js
@@ -6,5 +6,27 @@ console.log("World");
 string 
 numbers 
 boolean (yes or no, true or false)
+bigint
+
+undefined (no defination)
+null (empty)
+
+object
+
+symbol
 
 */
+
+var score = 102 //older format 
+
+let name = "shaurya" // newer format
+
+let teaType = ["Lemon tea", "orange tea", "colong tea"]
+let user = {firstname: "Shaurya" , lastname: "Patil"} 
+
+let getScore = score //grabs value from score
+
+console.log(getScore);// print on screen.
+
+
+
