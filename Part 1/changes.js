0aac4933b67c1ec6c2bf616bb7817fd349090417
Shaurya <shaurya.35k@gmail.com>
2025-10-26 21:53:08 +0530

    operations and changes

---
+++ Part 1/changes.js
@@ -0,0 +1,12 @@
+let gamename = "spiderman"
+
+gamename = "batman" //overwrites the spiderman 
+
+console.log(gamename);
+
+const username = "shauryaptil"
+
+console.log(username);
+
+
+
---
+++ Part 1/operations.js
@@ -0,0 +1,31 @@
+let addition = 4 + 5 //just usual simple addition.
+
+let score = 102
+let bonus = 25
+let total = score = bonus //same as line one but here we took 2 diff variables and then added it. 
+
+
+let add = 4 + 5 
+let sub = 4 - 5 
+let multi = 4 * 5
+let div = 4 / 5
+let reminder = 4 % 5
+let expo = 2 ** 3 // here we are calculating the exponents where the number after ** is the power and the number before ** is our base.
+
+console.log(add);
+console.log(sub);
+console.log(multi);
+console.log(div);
+console.log(reminder);
+console.log(expo);
+
+
+let myscore = 100
+myscore++ // here we incremented the value of myscore 
+
+
+let credit = 100
+credit-- // here we decremented the value of credit
+
+
+
